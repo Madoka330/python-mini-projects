fbc3d531de2a9b345c53408f3ebf8d70c3b8832a
Madoka330 <15984848144cx@gmail.com>
2025-12-23 21:14:50 +0800

    添加飞船

---
+++ alien_invasion/settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """游戏相关设置"""
+
+    def __init__(self) -> None:
+        """初始化游戏设置"""
+        
+        # 屏幕设置
+        self.screen_witdh = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
